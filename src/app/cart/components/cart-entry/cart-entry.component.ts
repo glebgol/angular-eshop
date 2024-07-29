aa2005166410de50dac77064a912a833aee73647
Gleb Golovnev <glebgolovnev2004@gmail.com>
2024-07-29 13:12:00 +0300

    Separate cart-entry template

--- src/app/cart/components/cart-entry/cart-entry.component.ts
+++ src/app/cart/components/cart-entry/cart-entry.component.ts
@@ -1,20 +1,9 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
-import {CartProductEntry} from "../../../shared/models/cart-product-entry";
+import {CartProductEntry} from "../../../shared/models/cart-product-entry.model";
 import {faSquareXmark} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
-  template: `
-    <th class="cart-table__cell--id">{{ cartEntry.id }}</th>
-    <td class="cart-table__cell--title">{{ cartEntry.title }}</td>
-    <td class="cart-table__cell--count">{{ cartEntry.count }}</td>
-    <td class="cart-table__cell--price">{{ cartEntry.price | currency }}</td>
-    <td class="cart-table__cell--total">{{ cartEntry.price * cartEntry.count | currency }}</td>
-    <td class="cart-table__cell--action">
-      <button class="cart-table__remove-btn" (click)="removeCartEntry(cartEntry.id)">
-        <fa-icon [icon]="removeIcon" class="cart-table__remove-icon"></fa-icon>
-      </button>
-    </td>
-  `,
+  templateUrl: 'cart-entry.component.html',
   styleUrl: 'cart-entry.component.scss',
   selector: '[appCartEntry]'
 })
